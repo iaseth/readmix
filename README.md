f19fe544ba1951bd7096f78a7bc5ed7d5dfc8781
Ankur Seth <iankurseth@gmail.com>
2023-04-25 13:01:01 +0530

    Added new filter: GithubRepoLink
    published readmix@0.6.4
    added shorted filter aliases GithubLink and NpmLink.

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 
 [`readmix`](https://www.npmjs.com/package/readmix) is a command-line README generator.
 
+You can find the source code at [`iaseth/readmix`](https://github.com/iaseth/readmix).
+
 
--- src/filters.ts
+++ src/filters.ts
@@ -1,10 +1,22 @@
 
 
 
+function GithubRepoLink (repoName: string): string {
+	return `[\`${repoName}\`](https://github.com/${repoName})`;
+}
+
 function NpmPackageLink (packageName: string): string {
 	return `[\`${packageName}\`](https://www.npmjs.com/package/${packageName})`;
 }
 
+
+// shorter aliases for long filters
+const NpmLink = NpmPackageLink;
+const GithubLink = GithubRepoLink;
+
 export const filters = {
-	NpmPackageLink
+	GithubLink,
+	GithubRepoLink,
+	NpmLink,
+	NpmPackageLink,
 };
