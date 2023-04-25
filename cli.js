e7baaac94cee531d1ecda077f6864d7b7a534703
Ankur Seth <iankurseth@gmail.com>
2023-04-25 09:33:41 +0530

    Added dummies: compileFile() and compileDirectory()
    published readmix@0.2.0

--- cli.js
+++ cli.js
@@ -5,9 +5,45 @@ const path = require("path");
 
 
 
+function compileDirectory (dirpath) {
+	console.log(`compileDirectory: ${dirpath}`);
+	const entries = fs.readdirSync(dirpath);
+	for (const entry of entries) {
+		if (entry.startsWith(".")) {
+			continue; // skip hidden files/dirs
+		}
+
+		const fullpath = path.join(dirpath, entry);
+		const stat = fs.lstatSync(fullpath);
+		if (stat.isDirectory()) {
+			compileDirectory(fullpath);
+		} else if (stat.isFile()) {
+			compileFile(fullpath);
+		}
+	}
+}
+
+function compileFile (pathArg) {
+	console.log(`compileFile: ${pathArg}`);
+}
+
 function main () {
-	const [,, inputPath, ...args] = process.argv;
-	console.log(`inputPath: ${inputPath}`);
+	const [,, ...args] = process.argv;
+	const flags = args.filter(a => a.startsWith("-"));
+
+	const pathArgs = args.filter(a => !a.startsWith("-"));
+	for (const pathArg of pathArgs) {
+		if (fs.existsSync(pathArg)) {
+			const stat = fs.lstatSync(pathArg);
+			if (stat.isDirectory()) {
+				compileDirectory(pathArg);
+			} else if (stat.isFile()) {
+				compileFile(pathArg);
+			}
+		} else {
+			console.log(`Path Not Found: ${pathArg}`);
+		}
+	}
 }
 
 main();
