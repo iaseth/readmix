2006af0c1a3e9d026b4fa42ce47fe8073d35b14b
Ankur Seth <iankurseth@gmail.com>
2023-04-25 13:25:15 +0530

    Added new filters: File and CodeFile
    Published readmix@0.6.6
    Inserted LICENSE text into README.

--- src/filters.ts
+++ src/filters.ts
@@ -1,5 +1,8 @@
+import fs from 'fs';
+import path from 'path';
 
 
+const a3 = "```\n";
 
 function GithubRepoLink (repoName: string): string {
 	return `[\`${repoName}\`](https://github.com/${repoName})`;
@@ -9,12 +12,29 @@ function NpmPackageLink (packageName: string): string {
 	return `[\`${packageName}\`](https://www.npmjs.com/package/${packageName})`;
 }
 
+function File (filepath: string): string {
+	if (fs.existsSync(filepath)) {
+		const contents = fs.readFileSync(filepath, {encoding:'utf8'});
+		return contents;
+	} else {
+		return `File not found: ${filepath}\n`;
+	}
+}
+
+function CodeFile (filepath: string): string {
+	const contents = `Contents of ${filepath}`;
+	return a3 + File(filepath) + a3;
+}
+
 
 // shorter aliases for long filters
 const NpmLink = NpmPackageLink;
 const GithubLink = GithubRepoLink;
 
 export const filters = {
+	File,
+	CodeFile,
+
 	GithubLink,
 	GithubRepoLink,
 	NpmLink,
