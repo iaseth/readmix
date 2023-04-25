131bbed380622a5dbc146ca5fdd37ed46e1d5b13
Ankur Seth <iankurseth@gmail.com>
2023-04-25 13:38:45 +0530

    Added global giveCredit() that allows users to credit readmix
    Published readmix@0.6.7

--- README.md
+++ README.md
@@ -29,4 +29,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
+## Credit
+
+This file was generated using [`readmix`](https://www.npmjs.com/package/readmix)
 
--- src/readmix.ts
+++ src/readmix.ts
@@ -1,12 +1,18 @@
-import {filters} from './filters';
 
+
+
+function giveCredit () : string {
+	return "## Credit\n\nThis file was generated using [`readmix`](https://github.com/iaseth/readmix).\n";
+}
 
 interface ReadmixType {
-	appName: string
+	appName: string,
+	giveCredit: () => string
 };
 
 export const Readmix: ReadmixType = {
-	appName: "Readmix"
+	appName: "Readmix",
+	giveCredit: giveCredit
 };
 
 
