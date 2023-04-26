25c02789e798b8ec23329cd3eb4eb3968d66b127
Ankur Seth <iankurseth@gmail.com>
2023-04-26 18:27:10 +0530

    Running a HelloWorld server in previewCommand

--- src/commands/preview.ts
+++ src/commands/preview.ts
@@ -1,9 +1,19 @@
+import http from 'http';
+
 import { EntryType } from "../helpers/rx";
 import { CmdOptionsType } from "./common";
 
-
+const hostname = '127.0.0.1';
+const port = 1996;
 
 export function previewCommand (entries: EntryType[], cmdOptions: CmdOptionsType) {
-	console.log(`Running previewCommand:`);
-	console.log("\t---- complete");
+	const server = http.createServer((req, res) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/plain');
+		res.end('Hello World');
+	});
+
+	server.listen(port, hostname, () => {
+		console.log(`Server running at http://${hostname}:${port}/`);
+	});
 }
