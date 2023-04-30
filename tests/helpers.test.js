707a330e94879260266a601bcf7d0ac982855441
Ankur Seth <iankurseth@gmail.com>
2023-04-30 21:03:57 +0530

    Added tests for isNotAComment and isSugar

--- tests/helpers.test.js
+++ tests/helpers.test.js
@@ -18,4 +18,33 @@ test("isComment", () => {
 	expect(helpers.isComment({text: "Foo"})).toBe(false);
 });
 
+test("isNotAComment", () => {
+	expect(helpers.isNotAComment({text: "// Comment"})).toBe(false);
+	expect(helpers.isNotAComment({text: "  // Comment"})).toBe(false);
+	expect(helpers.isNotAComment({text: "\t// Comment"})).toBe(false);
+
+	expect(helpers.isNotAComment({text: ":: Comment"})).toBe(false);
+	expect(helpers.isNotAComment({text: "  :: Comment"})).toBe(false);
+	expect(helpers.isNotAComment({text: "\t:: Comment"})).toBe(false);
+
+	expect(helpers.isNotAComment({text: "  "})).toBe(true);
+	expect(helpers.isNotAComment({text: "\t"})).toBe(true);
+	expect(helpers.isNotAComment({text: "$"})).toBe(true);
+	expect(helpers.isNotAComment({text: "Foo"})).toBe(true);
+});
+
+test("isSugar", () => {
+	expect(helpers.isSugar({text: "// Comment"})).toBe(false);
+	expect(helpers.isSugar({text: "  // Comment"})).toBe(false);
+	expect(helpers.isSugar({text: "\t// Comment"})).toBe(false);
+
+	expect(helpers.isSugar({text: ":: Comment"})).toBe(false);
+	expect(helpers.isSugar({text: "  :: Comment"})).toBe(false);
+	expect(helpers.isSugar({text: "\t:: Comment"})).toBe(false);
+
+	expect(helpers.isSugar({text: " $ Foo"})).toBe(true);
+	expect(helpers.isSugar({text: " => Foo"})).toBe(true);
+	expect(helpers.isSugar({text: " -> Foo"})).toBe(true);
+});
+
 
