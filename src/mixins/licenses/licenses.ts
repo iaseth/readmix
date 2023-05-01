a8d222f65e4d7d48a7bc431ccdc100a87b9a3f9b
Ankur Seth <iankurseth@gmail.com>
2023-05-01 22:45:33 +0530

    Added mixins gnuGplLicense and gnuLgplLicense

--- src/mixins/licenses/licenses.ts
+++ src/mixins/licenses/licenses.ts
@@ -1,6 +1,7 @@
 
 export { apache2License } from './apache';
 export { bsd2License, bsd3License } from './bsd';
+export { gnuGplLicense, gnuLgplLicense } from './gpl';
 export { iscLicense } from './isc';
 export { mitLicense } from "./mit";
 export { msplLicense } from './microsoft';
--- src/mixins/mixins.ts
+++ src/mixins/mixins.ts
@@ -1,6 +1,6 @@
 import { extras } from './extras';
 import { gh } from './gh';
-import { apache2License, bsd2License, bsd3License, iscLicense, mitLicense, msplLicense, zlibLicense } from './licenses';
+import { apache2License, bsd2License, bsd3License, gnuGplLicense, gnuLgplLicense, iscLicense, mitLicense, msplLicense, zlibLicense } from './licenses';
 import { giveCredit } from './meta';
 import { npm } from './npm';
 import { dependencyTable, devDependencyTable } from './packagejson';
@@ -23,6 +23,8 @@ export const mixins = {
 	apache2License,
 	bsd2License,
 	bsd3License,
+	gnuGplLicense,
+	gnuLgplLicense,
 	iscLicense,
 	mitLicense,
 	msplLicense,
