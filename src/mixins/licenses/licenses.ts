f0241ec2ca385022c775dd5932ba7e0544bddcc8
Ankur Seth <iankurseth@gmail.com>
2023-05-01 22:59:36 +0530

    Added eclipse licenses: epl1License and epl2License

--- src/mixins/licenses/licenses.ts
+++ src/mixins/licenses/licenses.ts
@@ -2,6 +2,7 @@
 export { apache2License } from './apache';
 export { bsd2License, bsd3License } from './bsd';
 export { cclLicense } from './ccl';
+export { epl1License, epl2License } from './eclipse';
 export { gnuGplLicense, gnuLgplLicense } from './gpl';
 export { iscLicense } from './isc';
 export { mitLicense } from "./mit";
--- src/mixins/mixins.ts
+++ src/mixins/mixins.ts
@@ -1,6 +1,6 @@
 import { extras } from './extras';
 import { gh } from './gh';
-import { apache2License, bsd2License, bsd3License, cclLicense, gnuGplLicense, gnuLgplLicense, iscLicense, mitLicense, mplLicense, msplLicense, zlibLicense } from './licenses';
+import { apache2License, bsd2License, bsd3License, cclLicense, epl1License, epl2License, gnuGplLicense, gnuLgplLicense, iscLicense, mitLicense, mplLicense, msplLicense, zlibLicense } from './licenses';
 import { giveCredit } from './meta';
 import { npm } from './npm';
 import { dependencyTable, devDependencyTable } from './packagejson';
@@ -24,6 +24,8 @@ export const mixins = {
 	bsd2License,
 	bsd3License,
 	cclLicense,
+	epl1License,
+	epl2License,
 	gnuGplLicense,
 	gnuLgplLicense,
 	iscLicense,
