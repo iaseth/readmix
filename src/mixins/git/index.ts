531c16c5386cca30ad658a14d90958b0bb415139
Ankur Seth <iankurseth@gmail.com>
2023-05-03 19:12:55 +0530

    Added empty git mixins module

---
+++ src/mixins/git/index.ts
@@ -0,0 +1,2 @@
+
+export { git } from "./git";
--- src/mixins/mixins.ts
+++ src/mixins/mixins.ts
@@ -1,5 +1,6 @@
 import { extras } from './extras';
 import { gh } from './gh';
+import { git } from './git';
 import { licenses } from './licenses';
 import { meta } from './meta';
 import { npm } from './npm';
@@ -19,8 +20,9 @@ export const mixins = {
 	...std,
 	...extras,
 
-	gh,
-	npm,
+	gh,   // GitHub mixins
+	git,  // git mixins
+	npm,  // npm/node mixins
 
 	// putting most important entries at the last to ensure they are not overwritten
 	std,
